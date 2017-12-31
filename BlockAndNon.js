19d825a43143214f6b33f44bacebbb359984f183
Josue Hernandez <jghg.02@gmail.com>
2017-12-31 20:07:29 -0300

    Added some coments.

--- BlockAndNon.js
+++ BlockAndNon.js
@@ -4,16 +4,22 @@ var satrt, finish;
 start = new Date();
 console.log('Start - Block');
 
+//Import Lib. 
 var fs = require('fs');
+
+//Blocking Code
+
+//Read file
 var fileRead = fs.readFileSync('text.txt','utf8');
 console.log(fileRead);
 
-
+//Get finish date
 finish = new Date();
 console.log('Execute Time: ' + (finish.getTime() - start.getTime()) + ' mili-seg');
 
 
 console.log('------------------------------');
+//Non-Blocking Code
 start = new Date();
 console.log('Start - NoNBlock');
 
